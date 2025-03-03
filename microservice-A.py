3daeb2c916d357c65a45ad125116c5ff02226718
“TheoB” <brunsont@oregonstate.edu>
2025-03-03 16:01:40 -0500

    Consolidated duplicate code into functions

--- microservice-A.py
+++ microservice-A.py
@@ -8,95 +8,94 @@ Created on Tue Feb 25 12:42:21 2025
 import zmq
 import json
 
+def printBlankSpaces(item_name, txt_file, spaces = 15):
+    """ Prints a set of blank spaces to the given text file"""
+    space_length = spaces - len(item_name)
+    for _ in range(space_length):
+        txt_file.write (' ')
+
+def printListHeader(list_name, txt_file):
+    """ Prints the List Name and Table Header to the given text file"""
+    txt_file.write('\n')
+    txt_file.write (list_name)
+    txt_file.write ('\n\n')
+    txt_file.write ('Item')
+    printBlankSpaces('Item', txt_file)
+    txt_file.write('Quantity')
+    printBlankSpaces('Quantity', txt_file)
+    txt_file.write('Priority\n')
+
+def printList(list_data, txt_file):
+    """ Prints all the items in the list by  their name, quantity and priority values to the given text file"""
+    for item in list_data:
+        txt_file.write (item['item'])
+        printBlankSpaces(item['item'], txt_file)
+
+        txt_file.write (item['quantity'])
+        printBlankSpaces(item['quantity'], txt_file)
+
+        txt_file.write (item['priority'])
+        txt_file.write ('\n')
 
 
-# ZeroMQ Server
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
+def main():
+    # ZeroMQ Server
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
 
-print('Microservice is running...')
+    print('Microservice is running...')
 
-while True:
+    while True:
 
-    # Wait for a request from the client
-    print('Waiting for request from client...')
-    message = socket.recv()
-    
-    # name of list they want printed to txt
-    list_name = message.decode('utf-8')
-    
-    print('Received request')
-    
-    # open json file
-    json_file = open('shopping_lists.json')
-    data = json.load(json_file)
-    
-    # open txt file
-    txt_file = open('list_export.txt', 'w')
-    
-    list_found = False
-    
-    # checks if a valid list name was given
-    for list in data: 
+        # Wait for a request from the client
+        print('Waiting for request from client...')
+        message = socket.recv()
         
-        if list == list_name:
-            
-            # if they find the list, write it to txt
-            list_found = True
-            txt_file.write (list_name)
-            txt_file.write ('\n\n')
-            txt_file.write ('Item          Quantity          Priority\n')
-
-            for item in data[list]:
-                txt_file.write (item['item'])
-                length = len(item['item'])
-                space_length = 15 - length
-                for i in range(space_length):
-                    txt_file.write (' ')
-                txt_file.write (item['quantity'])
-                length = len(item['quantity'])
-                space_length = 15 - length
-                for i in range(space_length):
-                    txt_file.write (' ')
-                txt_file.write (item['priority'])
-                txt_file.write ('\n')
-            
-    if list_found is False:
+        # name of list they want printed to txt
+        list_name = message.decode('utf-8')
         
-        # if list name wasnt found, prints out all lists
-        for list in data:
-            #print name of list
-            txt_file.write ('\n')
-            txt_file.write (list)
-            txt_file.write ('\n\n')
-            txt_file.write ('Item          Quantity        Priority\n')
+        print('Received request')
         
+        # open json file
+        json_file = open('shopping_lists.json')
+        data = json.load(json_file)
+        
+        # open txt file
+        txt_file = open('list_export.txt', 'w')
+        
+        list_found = False
+        
+        # checks if a valid list name was given
+        for list in data: 
             
-            for item in data[list]:
-                txt_file.write (item['item'])
-                length = len(item['item'])
-                space_length = 15 - length
-                for i in range(space_length):
-                    txt_file.write (' ')
-                txt_file.write (item['quantity'])
-                length = len(item['quantity'])
-                space_length = 15 - length
-                for i in range(space_length):
-                    txt_file.write (' ')
-                txt_file.write (item['priority'])
-                txt_file.write ('\n')
+            if list == list_name:
                 
-    txt_file.close()
+                # if they find the list, write it to txt
+                list_found = True
+                printListHeader(list_name, txt_file)
+                printList(data[list], txt_file)
+                break
                 
-    # Send response back to client
-    response = "JSON exported successfully."
-    socket.send(response.encode("utf-8"))
+        if list_found is False:
+            
+            # if list name wasnt found, prints out all lists
+            for list in data:
+                #print name of list
+                printListHeader(list, txt_file)          
+                printList(data[list], txt_file)
+                    
+        txt_file.close()
+                    
+        # Send response back to client
+        response = "JSON exported successfully."
+        socket.send(response.encode("utf-8"))
 
 
+if __name__ == "__main__":
+    main()
 
 
-
-    
+        
 
 
